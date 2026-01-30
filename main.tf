c69436136a074e3b549e3398a56820f00a1db819
ij630-a11y <ijazamna630@gmail.com>
2026-01-30 13:55:32 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,13 +9,13 @@ terraform {
 
 provider "azurerm" {
   features {}
-  subscription_id = "<your subscription id here>"
+  subscription_id = "6d3ab529-5798-406f-a3eb-5573b8d32ed2"
 }
 
 terraform {
   backend "azurerm" {
     resource_group_name  = "RG1"
-    storage_account_name = "<your storage account>"
+    storage_account_name = "jenkinsstateamna"
     container_name       = "terraform-state"
     key                  = "terraform.tfstate"
   }
